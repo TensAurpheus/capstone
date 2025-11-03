9498bd90a98dc3ad4ac41a1fab0a4139ae8f8f78
Yana Mudruk <atlantidas84@gmail.com>
2025-11-03 23:03:58 +0100

    removed standardize

--- src/data_pipeline/data/__init__.py
+++ src/data_pipeline/data/__init__.py
@@ -10,14 +10,12 @@ Handles preprocessing, normalization, standardization, and postprocessing.
 
 from .preprocessing import preprocess
 from .normalize import normalize_features
-from .standardize import standardize_features
 from .data_postprocess import prepare_dataframe_for_model
-from src.data_pipeline.features.technical import add_technical_indicators  # ✅ правильний імпорт із features
+from src.data_pipeline.features.technical import add_technical_indicators 
 
 __all__ = [
     "preprocess",
     "normalize_features",
-    "standardize_features",
     "prepare_dataframe_for_model",
     "add_technical_indicators",
 ]
